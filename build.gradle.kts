bd2c1b750ce9c584b450fff7a75253a1ad8be90c
DemonWav <demonwav@gmail.com>
2017-03-25 01:51:19 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,89 @@
+import org.gradle.api.Task
+import org.gradle.api.tasks.JavaExec
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        maven {
+            name = "intellij-plugin-service"
+            setUrl("https://dl.bintray.com/jetbrains/intellij-plugin-service")
+        }
+    }
+}
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.1.1"
+    idea
+    id("org.jetbrains.intellij") version "0.2.5"
+}
+
+val CI = System.getenv("CI") != null
+
+val ideaVersion: String by extra
+val javaVersion: String by extra
+val downloadIdeaSources: String by extra
+
+val runIde: JavaExec by tasks
+val compileKotlin by tasks
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlinModule("stdlib-jre8")) {
+        // JetBrains annotations are already bundled with IntelliJ IDEA
+        exclude(group = "org.jetbrains", module = "annotations")
+    }
+
+    // Add an additional dependency on kotlin-runtime. It is essentially useless
+    // (since kotlin-runtime is a transitive dependency of kotlin-stdlib-jre8)
+    // but without kotlin-stdlib or kotlin-runtime on the classpath,
+    // gradle-intellij-plugin will add IntelliJ IDEA's Kotlin version to the
+    // dependencies which conflicts with our newer version.
+    compile(kotlinModule("runtime")) {
+        isTransitive = false
+    }
+
+    compile("org.spongepowered:Despector:0.1.0-SNAPSHOT")
+}
+
+intellij {
+    // IntelliJ IDEA dependency
+    version = ideaVersion
+
+    pluginName = "Despector Integration"
+    updateSinceUntilBuild = false
+
+    downloadSources = !CI && downloadIdeaSources.toBoolean()
+
+    sandboxDirectory = project.rootDir.canonicalPath + "/.sandbox"
+}
+
+java {
+    setSourceCompatibility(javaVersion)
+    setTargetCompatibility(javaVersion)
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = javaVersion
+}
+
+idea {
+    module {
+        excludeDirs.add(file(intellij().sandboxDirectory))
+    }
+}
+
+runIde {
+    findProperty("intellijJre")?.let(this::setExecutable)
+
+    System.getProperty("debug")?.let {
+        systemProperty("idea.ProcessCanceledException", "disabled")
+        systemProperty("idea.debug.mode", "true")
+    }
+}
+
+inline operator fun <T : Task> T.invoke(a: T.() -> Unit): T = apply(a)
+fun KotlinDependencyHandler.kotlinModule(module: String) = kotlinModule(module, "1.1.1") as String
---
+++ src/main/kotlin/com/demonwav/despector/DespectorFileDecompiler.kt
@@ -0,0 +1,49 @@
+package com.demonwav.despector
+
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.compiled.ClassFileDecompilers
+import org.spongepowered.despector.Despector
+
+class DespectorFileDecompiler : ClassFileDecompilers.Light() {
+
+    override fun accepts(file: VirtualFile) = file.extension == "class"
+
+    @Throws(CannotDecompileException::class)
+    override fun getText(file: VirtualFile): CharSequence {
+        try {
+            val stream = file.inputStream
+            return BANNER + Despector.decompile(stream) { internalName ->
+                val name = internalName.split("/").toTypedArray()
+                val firstDir = name[0]
+
+                var dir = file.parent
+
+                while (dir.name != firstDir) {
+                    dir = dir.parent
+
+                    if (dir == null) {
+                        return@decompile null
+                    }
+                }
+
+                // One more time, get it's parent so we can find the class relative to it
+                dir = dir.parent
+
+                name[name.lastIndex] = name[name.lastIndex] + ".class"
+                return@decompile VfsUtil.findRelativeFile(dir, *name)?.inputStream
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw CannotDecompileException(e)
+        }
+    }
+
+    companion object {
+        private const val BANNER =
+            "\n" +
+            "  // Despector decompiled source generated from a class file\n" +
+            "  // https://github.com/Deamon5550/Despector" +
+            "\n\n"
+    }
+}
